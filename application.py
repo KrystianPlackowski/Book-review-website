f80702ac5d8b0fe05821cf5c0a231bf4ca342423
KPlackowski <kpchris20@gmail.com>
2019-12-18 14:55:26 +0100

    fix

--- application.py
+++ application.py
@@ -4,6 +4,7 @@ from flask import Flask, session, render_template, request, json
 from flask_session import Session
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
+from help_functions import *
 
 app = Flask(__name__)
 
@@ -29,37 +30,18 @@ def index():
 def search():
     return render_template("search.html")
 
-
-def perform_search(query_type, query):
-    if query_type == 'isbn':
-        return db.execute("SELECT * FROM books WHERE isbn LIKE :query", {"query":'%'+query+'%'}).fetchall()
-    
-    if query_type == 'title':
-        return db.execute("SELECT * FROM books WHERE title LIKE :query", {"query":'%'+query+'%'}).fetchall()
-
-    if query_type == 'author':
-        return db.execute("SELECT * FROM books WHERE author LIKE :query", {"query":'%'+query+'%'}).fetchall()
-
-    if query_type == 'year':
-        return db.execute("SELECT * FROM books WHERE year = :query", {"query":int(query)}).fetchall()
-
-
 @app.route('/search/results', methods=["POST"])
 def results():
     query = request.form.get("search query")
-    query_type = request.form.get("query type")
 
     # Perform query for each typed word (separated by spaces) and return 
-    # sum of query results (without repeatings), sorted by ID
+    # an UNION of all query results (results must contain ALL words somewhere), sorted by ID
 
-    query_results = set()
     for single_query in query.split(' '):
-        list_of_proxy = perform_search(query_type, single_query)
-
-        # Can't hash using sqlAlchemy's 'tuplelike' type, so convert to tuples
-
-        list_of_tuples = [tuple(list(i)) for i in list_of_proxy] 
-        query_results |= set(list_of_tuples)
+        if single_query == query.split(' ')[0]:
+            query_results = perform_search(db, single_query)
+        else:
+            query_results &= perform_search(db, single_query)
 
     #return json.dumps(query.split(' '))
     return render_template("results.html", results=sorted(list(query_results)))

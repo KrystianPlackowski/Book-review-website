d843e6fda8e5e2cc77b3c29c043fb2de9e753f11
KPlackowski <kpchris20@gmail.com>
2019-12-18 23:20:00 +0100

    New features (i.e. login/regiter)

--- application.py
+++ application.py
@@ -30,13 +30,81 @@ db = scoped_session(sessionmaker(bind=engine))
 api_key = os.getenv("BOOK_API_KEY")
 
 
-@app.route('/')
+@app.route('/', methods=["GET", "POST"])
 def index():
-    return "Welcome page"
+    # Initiate global variable `login`
+    if session.get("login") is None:
+        session["login"] = None
 
-@app.route('/search')
+    # If user already logged in
+    if session["login"] != None:
+        return render_template("index.html", login=session["login"])
+
+    # If user not logged in and got to the page by typing url
+    if request.method == "GET":
+        return render_template("login.html", message=
+                "In order to perform a search please log in.")
+
+    # Otherwise check if provided credentials are correct
+    # (method is "POST" and user not logged in)
+    login = request.form.get("login")
+    password = request.form.get("password")
+
+    cred = db.execute("SELECT * FROM users WHERE login = :login "
+            + "AND password = :password"
+            , {"login":login, "password":password}).fetchone()
+     
+    # Credentials not found
+    if cred == None:
+        return render_template("login.html"
+                , message="Wrong credentials. Please try again.")
+
+    # Successful login!
+    session["login"] = login
+    return render_template("index.html", login=session["login"])
+
+
+@app.route('/logout', methods=["POST"])
+def logout():
+    session["login"] = None
+    return render_template("login.html", message="Successfully logged out.")
+
+
+@app.route('/register', methods=["GET", "POST"])
+def register():
+    # If got to this page without providing credentials in form
+    if request.method == "GET":
+        return render_template("register.html"
+                , message="Enter desired username and password.")
+    
+    # If after providing credentials in `register.html` form
+    login = request.form.get("login")
+    password = request.form.get("password")
+
+    # If inccorect username or password
+    if len(login) < 4 or len(password) < 4:
+        return render_template("register.html",
+                message="Invalid username or password! At least 4 characters needed for each.")
+                    
+    # If username already taken
+    cred = db.execute("SELECT * FROM users WHERE login = :login"
+            , {"login":login}).fetchone()
+    if cred != None:
+        return render_template("register.html"
+                , message="Username already taken!")
+
+    # If everything was OK
+    db.execute("INSERT INTO users (login, password) "
+    + "VALUES (:login, :password)", {"login":login, "password":password})
+    db.commit()
+
+    return render_template("login.html", message="Successfully registered. Please log in.")
+
+
+@app.route('/search', methods=["POST"])
 def search():
     return render_template("search.html")
+
 
 @app.route('/search/results', methods=["POST"])
 def results():
@@ -53,6 +121,7 @@ def results():
 
     return render_template("results.html", results=sorted(list(query_results)))
 
+
 @app.route('/search/results/<int:book_id>')
 def book_info(book_id):
     book = db.execute("SELECT * FROM books WHERE ID = :book_id"
@@ -65,4 +134,4 @@ def book_info(book_id):
             , params={"key": api_key, "isbns": book.isbn})
     
     return render_template("book_info.html", book=book
-            , res=res.json()['books'][0])
+            , res=res.json()['books'][0], code=res.status_code)
